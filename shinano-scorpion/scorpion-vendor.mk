5a40e509f3a4d9e6df4f634de7171e16efad8eeb
Kousuke Nagatsuka <koron393@gmail.com>
2016-11-11 22:40:36 +0900

    scorpion: Inherit qcom proprietary blobs

--- shinano-scorpion/scorpion-vendor.mk
+++ shinano-scorpion/scorpion-vendor.mk
@@ -1,5 +1,6 @@
 PRODUCT_PLATFORM := shinano
 $(call inherit-product, vendor/nxp/nxp-vendor.mk)
 $(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
+$(call inherit-product, vendor/qcom/proprietary/qcom-proprietary.mk)
 $(call inherit-product, vendor/sony/shinano-common/shinano-partial.mk)
 $(call inherit-product, $(LOCAL_PATH)/scorpion-partial.mk)
