38c7a179f622f7698db0b8d9e75dc30d99d743b5
Kousuke Nagatsuka <koron393@gmail.com>
2017-07-17 19:14:00 +0900

    scorpion: Move BCM Wi-Fi firmware to device tree

--- shinano-scorpion/scorpion-vendor.mk
+++ shinano-scorpion/scorpion-vendor.mk
@@ -1,6 +1,5 @@
 PRODUCT_PLATFORM := shinano
 $(call inherit-product, vendor/nxp/nxp-vendor.mk)
-$(call inherit-product, vendor/broadcom/wlan/bcmdhd/firmware/bcm4354/device-bcm.mk)
 $(call inherit-product, vendor/qcom/prebuilt/qcom-vendor.mk)
 $(call inherit-product, vendor/qcom/proprietary/qcom-proprietary.mk)
 $(call inherit-product, vendor/sony/shinano-common/shinano-partial.mk)
